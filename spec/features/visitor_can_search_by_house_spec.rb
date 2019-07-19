0da94f9ece40620c16d7d9da67ac955d802f0494
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-19 11:52:03 -0600

    Remove old comments

--- spec/features/visitor_can_search_by_house_spec.rb
+++ spec/features/visitor_can_search_by_house_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe 'visitor searches for members of a house' do
       find('#house').select('Greyjoy')
       click_button 'Get Members'
 
-      # Then my path should be "/search" with "house=greyjoy" in the parameters
       expect(current_path).to eq('/search')
 
       within('h1') do
@@ -20,11 +19,9 @@ RSpec.describe 'visitor searches for members of a house' do
 
       expect(page).to have_content('7 Members')
 
-      # And I should see a list of the 7 members of House Greyjoy
       expect(page.all('.member-list').count).to eq(7)
 
       within(page.first('.member-list')) do
-        # And I should see a name and id for each member.
         expect(page).to have_css('.member-name')
         expect(page).to have_css('.member-id')
       end
