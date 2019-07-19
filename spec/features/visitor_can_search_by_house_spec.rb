649bf925b22093cdd05991554059c69ac59a03f3
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-19 11:27:41 -0600

    Use VCR for feature test

--- spec/features/visitor_can_search_by_house_spec.rb
+++ spec/features/visitor_can_search_by_house_spec.rb
@@ -2,26 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'visitor searches for members of a house' do
   it 'shows house members' do
-    visit '/'
+    VCR.use_cassette('visitor_search_house_members', record: :new_episodes) do
+      visit '/'
 
-    expected_houses = ['Stark', 'Lannister', 'Targaryen', 'Tyrell', 'Greyjoy']
-    expect(page).to have_select('house', with_options: expected_houses)
+      expected_houses = ['Stark', 'Lannister', 'Targaryen', 'Tyrell', 'Greyjoy']
+      expect(page).to have_select('house', with_options: expected_houses)
 
-    find('#house').select('Greyjoy')
-    click_button 'Get Members'
+      find('#house').select('Greyjoy')
+      click_button 'Get Members'
 
-    # Then my path should be "/search" with "house=greyjoy" in the parameters
-    expect(current_path).to eq('/search')
+      # Then my path should be "/search" with "house=greyjoy" in the parameters
+      expect(current_path).to eq('/search')
 
-    expect(page).to have_content('7 Members')
+      expect(page).to have_content('7 Members')
 
-    # And I should see a list of the 7 members of House Greyjoy
-    expect(page.all('.member-list').count).to eq(7)
+      # And I should see a list of the 7 members of House Greyjoy
+      expect(page.all('.member-list').count).to eq(7)
 
-    within(page.first('.member-list')) do
-      # And I should see a name and id for each member.
-      expect(page).to have_css('.member-name')
-      expect(page).to have_css('.member-id')
+      within(page.first('.member-list')) do
+        # And I should see a name and id for each member.
+        expect(page).to have_css('.member-name')
+        expect(page).to have_css('.member-id')
+      end
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,17 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'vcr'
+require 'webmock/rspec'
+
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data('<WESTEROS_AAS_API_KEY>') { ENV['WESTEROS_AAS_API_KEY'] }
+  config.filter_sensitive_data('<WESTEROS_API_KEY>') { ENV['WESTEROS_API_KEY'] }
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
