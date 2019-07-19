d0b660a1f92b4d20fa9799161fa1c91838bd284e
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-19 11:19:55 -0600

    Add tests of WesterosAasApiService#members & #house_list

--- spec/services/westeros_aas_api_service_spec.rb
+++ spec/services/westeros_aas_api_service_spec.rb
@@ -1,10 +1,27 @@
-require 'spec_helper'
-require './app/services/westeros_aas_api_service'
+require 'rails_helper'
 
 RSpec.describe WesterosAasApiService do
+  before(:each) do 
+    @service = WesterosAasApiService.new
+  end
+
   it 'exists' do
-    service = WesterosAasApiService.new
-    
-    expect(service).to be_a(WesterosAasApiService)
+    expect(@service).to be_a(WesterosAasApiService)
+  end
+
+  it '#members gets data for members of a particular house (given an ID)' do
+    members = @service.members(5)
+
+    expect(members.count).to eq(7)
+    expect(members.first).to have_key(:name)
+    expect(members.first).to have_key(:id)
+  end
+  
+  it '#house_list lists the names (and IDs) of all houses' do
+    house_list = @service.house_list
+  
+    expect(house_list.count).to eq(5)
+    expect(house_list.first).to have_key(:name)
+    expect(house_list.first).to have_key(:id)
   end
 end
